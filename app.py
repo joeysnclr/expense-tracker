af53d107496ff42252433374f4888158452e8c11
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-04-16 15:30:50 -0700

    random commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-import json, os
+import json, os, api
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', i=api.randomNum())
 
 
-app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=port, debug=True)
